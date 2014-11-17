faa6ecfc803ed539cb25c42fb8b1fd2b2c730aff
Alex Sanchez <ansanchezdesign@gmail.com>
2014-11-16 21:04:18 -0500

    initial commit: what the site looked like when I made this github project

---
+++ Gruntfile.js
@@ -0,0 +1,67 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		// sass: {
+		// 	dist: {
+		// 		options: {
+		// 			style: 'nested'
+		// 		},
+		// 		files: {
+		// 			'css/base.css': 'css/base.scss'
+		// 		},
+		// 	},
+		// },
+
+		// autoprefixer: {
+		// 	dist: {
+		// 		files: {
+		// 			'css/style.css': 'css/base.css'
+		// 		},
+		// 	},
+		// },
+
+		connect: {
+			server: {
+				options: {
+					port: 9000,
+					hostname: 'localhost',
+					livereload: true,
+					open: true
+				},
+			},
+		},
+
+		watch: {
+			options: {
+				livereload: true,
+			},
+			html: {
+				files: ['*.html'],
+				options: {
+					spawn: false,
+				},
+			},
+			css: {
+				files: ['css/*.css'],
+				// tasks: ['sass', 'autoprefixer'],
+				options: {
+					spawn: false,
+				},
+			},
+			// script: {
+			// 	files: ['js/*.js'],
+			// 	options: {
+			// 		spawn: false,
+			// 	},
+			// },
+		},
+	});
+
+	// grunt.loadNpmTasks('grunt-contrib-sass');
+	// grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', [/*'sass', 'autoprefixer', */'connect', 'watch']);
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# alexisnsanchez.com
+
+Hi, my name is [Alex](http://www.alexisnsanchez.com). This github project holds the contents and code to my personal website.
+
+Visit it at [alexisnsanchez.com](http://www.alexisnsanchez.com).
+
+### Credit where credit is due
+
+- [jQuery](http://jquery.com)
+- [Sass](http://sass-lang.com)
+- [Grunt](http://gruntjs.com)
+- [Isotope](http://isotope.metafizzy.co/)
+- [Google Fonts](http://google.com/fonts)
